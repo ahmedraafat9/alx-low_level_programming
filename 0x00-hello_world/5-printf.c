8c3e41960b17e4622ea4a42fcbe21467d98e8801
ahmed <ahmed@example.com>
2023-02-20 09:57:19 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: 'the program's description'
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* main function implementation goes here*/
+	printf("with proper grammar, but the outcome is a piece of art,");
+	/*This program returns 0 if it completes successfully*/
+	return (0);
+}
